9c1dee5daf334c2f3c043d5bf510d170b45ea60a
ArchanaaS_17 <94780258+ArchanaaS71@users.noreply.github.com>
2026-02-13 13:15:55 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+
+from pypdf import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import FAISS
+from langchain.embeddings import HuggingFaceEmbeddings
+from transformers import pipeline
+from langchain.llms import HuggingFacePipeline
+from langchain.chains.summarize import load_summarize_chain
